91b1e3f5a379bd961c03f68cdf03b4d5aa3121ba
shrey <shreya272002gupta@gmail.com>
2025-02-24 16:08:20 +0530

    [Shreya] Add. Greeting program in UC4

---
+++ src/main/java/com/example/myGreetingApp/model/Greeting.java
@@ -0,0 +1,36 @@
+package com.example.myGreetingApp.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Greeting {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String message;
+
+    public Greeting() {
+    }
+
+    public Greeting(String message) {
+        this.message = message;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
