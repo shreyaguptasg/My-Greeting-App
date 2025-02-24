1858da918e7c89380786ddf857d2c65f7684c613
shrey <shreya272002gupta@gmail.com>
2025-02-24 12:19:52 +0530

    [Shreya] Updadte. Greeting controller program in UC2

--- src/main/java/com/example/myGreetingApp/controller/GreetingController.java
+++ src/main/java/com/example/myGreetingApp/controller/GreetingController.java
@@ -1,15 +1,23 @@
 package com.example.myGreetingApp.controller;
 
+import com.example.myGreetingApp.service.GreetingService;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/greeting")
 public class GreetingController {
 
+    private final GreetingService greetingService;
+
+    public GreetingController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     @GetMapping
     public String getGreeting() {
-        return "{\"message\": \"Hello, this is a GET request!\"}";
+        return greetingService.getGreetingMessage();
     }
+
 
     @PostMapping
     public String postGreeting() {
