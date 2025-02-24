2402c687bdee789c4a0efabaf20c135e73fe7bf8
shrey <shreya272002gupta@gmail.com>
2025-02-24 12:36:14 +0530

    [Shreya] Update. Greeting service program in UC3

--- src/main/java/com/example/myGreetingApp/service/GreetingService.java
+++ src/main/java/com/example/myGreetingApp/service/GreetingService.java
@@ -8,4 +8,16 @@ public class GreetingService {
     public String getGreetingMessage() {
         return "{\"message\": \"Hello World\"}";
     }
+
+    public String getGreetingMessage(String firstName, String lastName) {
+        if (firstName != null && lastName != null) {
+            return String.format("{\"message\": \"Hello, %s %s!\"}", firstName, lastName);
+        } else if (firstName != null) {
+            return String.format("{\"message\": \"Hello, %s!\"}", firstName);
+        } else if (lastName != null) {
+            return String.format("{\"message\": \"Hello, %s!\"}", lastName);
+        } else {
+            return "{\"message\": \"Hello World!\"}";
+        }
+    }
 }
